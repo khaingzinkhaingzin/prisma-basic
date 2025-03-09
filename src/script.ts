5edfbb2c65b8a54fa44ee19816fb0ea2cb4b0e19
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2025-03-09 15:45:29 +0630

    ✅ [FINISH] user createMany and his writtenPosts created

--- src/script.ts
+++ src/script.ts
@@ -5,31 +5,48 @@ const prisma = new PrismaClient({ log: ["query"] });
 async function main() {
   // ... you will write your Prisma Client queries here
   await prisma.user.deleteMany();
-  // await prisma.userPreference.deleteMany();
-  const user = await prisma.user.create({
-    data: {
-      name: "Kyle",
-      email: "kyle@test.com",
-      age: 27,
-      userPreference: {
-        create: {
-          emailUpdates: true,
-        },
+  await prisma.userPreference.deleteMany();
+  await prisma.post.deleteMany();
+  const user = await prisma.user.createMany({
+    data: [
+      {
+        name: "Kyle",
+        email: "kyle@test.com",
+        age: 27,
       },
-    },
-    // get relation info
-    // include: {
-    //   userPreference: true,
-    // },
-    select: {
-      name: true,
-      userPreference: {
-        select: {
-          emailUpdates: true,
-        },
+      {
+        name: "Sally",
+        email: "sally@test.com",
+        age: 29,
       },
-    },
+    ],
   });
+  const kyle = await prisma.user.findUnique({
+    where: { email: "kyle@test.com" },
+  });
+  const sally = await prisma.user.findUnique({
+    where: { email: "sally@test.com" },
+  });
+
+  if (kyle) {
+    await prisma.post.create({
+      data: {
+        title: "Post 1",
+        averateRating: 3.5,
+        authorId: kyle.id,
+      },
+    });
+  }
+  if (sally) {
+    await prisma.post.create({
+      data: {
+        title: "Post 2",
+        averateRating: 3.7,
+        authorId: sally.id,
+      },
+    });
+  }
+
   console.log(user);
 }
 
