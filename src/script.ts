d15da8870adf3949e9ea2f59bcdfadca897c27b8
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2025-03-09 15:52:32 +0630

    ✨ [FINISH] findUnique to find with unique column

--- src/script.ts
+++ src/script.ts
@@ -4,48 +4,15 @@ const prisma = new PrismaClient({ log: ["query"] });
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  await prisma.user.deleteMany();
-  await prisma.userPreference.deleteMany();
-  await prisma.post.deleteMany();
-  const user = await prisma.user.createMany({
-    data: [
-      {
-        name: "Kyle",
-        email: "kyle@test.com",
-        age: 27,
-      },
-      {
-        name: "Sally",
-        email: "sally@test.com",
-        age: 29,
-      },
-    ],
+  const user = await prisma.user.findUnique({
+    where: {
+      // age_name: {
+      //   age: 27,
+      //   name: "Kyle",
+      // },
+      email: "kyle@test.com",
+    },
   });
-  const kyle = await prisma.user.findUnique({
-    where: { email: "kyle@test.com" },
-  });
-  const sally = await prisma.user.findUnique({
-    where: { email: "sally@test.com" },
-  });
-
-  if (kyle) {
-    await prisma.post.create({
-      data: {
-        title: "Post 1",
-        averateRating: 3.5,
-        authorId: kyle.id,
-      },
-    });
-  }
-  if (sally) {
-    await prisma.post.create({
-      data: {
-        title: "Post 2",
-        averateRating: 3.7,
-        authorId: sally.id,
-      },
-    });
-  }
 
   console.log(user);
 }
